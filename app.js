7b872dea7a7cfb836ef9ebb95ba14c776584825f
Wojtek Augustynski <wojtek.augustynski@comcast.net>
2015-08-09 12:33:54 -0700

    proxy not picking up; trying in host

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
+var httpProxy    = require('http-proxy');
+var http         = require('http');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var publicPath = path.resolve(__dirname,'public');
+var inProduction = process.env.NODE_ENV === 'production';
 
+// var routes = require('./routes/index');
+
+var proxy = httpProxy.createProxyServer({
+  changeOrigin: true,
+  ws: true
+});
+var port = inProduction ? process.env.PORT : 3000;
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,31 +26,30 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(publicPath));
 
-app.use('/', routes);
-app.use('/users', users);
+// app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
 
 // production error handler
 // no stacktraces leaked to user
@@ -56,5 +61,38 @@ app.use(function(err, req, res, next) {
   });
 });
 
+if (!inProduction) {
+  var bundle = require('./server/bundle.js');
+  bundle();
+
+  app.all('/build/*', function (req, res) {
+    proxy.web(req, res, {
+      target: 'http://127.0.0.1:3001'
+    });
+   });
+
+   app.all('/socket.io*', function( req, res) {
+     proxy.web(req, res, {
+       target: 'http://127.0.0.1:3001'
+     });
+   });
+  proxy.on('error', function(e){
+    console.log('Could not connect to proxy');
+  });
+   var server = http.createServer(app);
+
+   server.on('upgrade', function(req, socket, head) {
+     proxy.ws(req, socket, head);
+   });
+
+  server.listen(port, function() {
+    console.log('Server running on port ' + port);
+  });
+}
+
+
+// app.listen(port, function() {
+//   console.log('Server running on port ' + port);
+// });
 
 module.exports = app;
